104fe1f1e6f1ed9096c5d22d0b7814bb7a2e026a
kguido798 <g.kayigamba@alustudent.com>
2025-10-16 19:23:33 +0200

    mm

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,9 @@
-mport { loadRecords, saveRecords } from './storage.js';
+import { loadRecords, saveRecords } from './storage.js';
 import { renderTable } from './ui.js';
-
 let records = loadRecords();
 renderTable(records);
-
-// Handle new transaction form
 document.querySelector('#add-form').addEventListener('submit', e => {
   e.preventDefault();
-  // validate input here
-  // push new record into records array
   saveRecords(records);
   renderTable(records);
 });
-
