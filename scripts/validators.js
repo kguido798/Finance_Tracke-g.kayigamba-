b710bb8919a7b7bb8d4528c719e80ff5f065bfce
kguido798 <g.kayigamba@alustudent.com>
2025-10-16 19:25:51 +0200

    aa

--- scripts/validators.js
+++ scripts/validators.js
@@ -1,7 +1,5 @@
-
-export const reDescription = /^\S(?:.*\S)?$/; // no leading/trailing space
-export const reAmount = /^(0|[1-9]\d*)(\.\d{1,2})?$/; // valid number
+export const reDescription = /^\S(?:.*\S)?$/;
+export const reAmount = /^(0|[1-9]\d*)(\.\d{1,2})?$/;
 export const reDate = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
 export const reCategory = /^[A-Za-z]+(?:[ -][A-Za-z]+)*$/;
-export const reDuplicateWord = /\b(\w+)\s+\1\b/i; // advanced back-reference
-
+export const reDuplicateWord = /\b(\w+)\s+\1\b/i;
